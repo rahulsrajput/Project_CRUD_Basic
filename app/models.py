4e7c5b0d720f1ec2ba45f4ff82d21f544dc637f2
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-26 13:33:51 +0530

    created customer model & registered

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    address = models.TextField()
