eac643b37cba32499ceb4ad55005e20e60dda8fa
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-26 13:30:38 +0530

    home view setup & html form

--- app/views.py
+++ app/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        first = request.POST['InputFirst']
+        last = request.POST['InputLast']
+        phone = request.POST['InputPhone']
+        address = request.POST['InputAddress']
+        
+        # print(f'{first},{last},{phone},{address}')
+
+        
     return render(request,'home.html')
 
 
