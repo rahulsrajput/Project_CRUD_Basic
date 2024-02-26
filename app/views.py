1ceceaffa3c9c91369071da1f3e96d8e812a6624
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-26 13:06:36 +0530

    setup home page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+def update(request, id):
+    pass
+
+
+def delete(request, id):
+    pass
